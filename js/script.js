0af7a6031a9357a6dba237a784697f2a00e844d6
Jim Musyoki <jim.musyoki@strathmore.edu>
2021-04-22 14:42:19 +0300

    complete samsung pages

--- js/script.js
+++ js/script.js
@@ -33,5 +33,57 @@ $(document).ready(function() {
       $(".clickable_2").fadeIn("slow");
     });
 
+    $(".first").mouseover(function()
+    {
+      $("#hide_first").fadeIn("slow");
+    });
+    $(".first").mouseleave(function()
+    {
+      $("#hide_first").fadeOut("slow");
+    });
 
+    $(".second").mouseover(function()
+    {
+      $("#hide_second").fadeIn("slow");
+    });
+    $(".second").mouseleave(function()
+    {
+      $("#hide_second").fadeOut("slow");
+    });
+
+    $(".third").mouseover(function()
+    {
+      $("#hide_third").fadeIn("slow");
+    });
+    $(".third").mouseleave(function()
+    {
+      $("#hide_third").fadeOut("slow");
+    });
+
+    $(".fourth").mouseover(function()
+    {
+      $("#hide_fourth").fadeIn("slow");
+    });
+    $(".fourth").mouseleave(function()
+    {
+      $("#hide_fourth").fadeOut("slow");
+    });
+
+    $(".fifth").mouseover(function()
+    {
+      $("#hide_fifth").fadeIn("slow");
+    });
+    $(".fifth").mouseleave(function()
+    {
+      $("#hide_fifth").fadeOut("slow");
+    });
+
+    $(".sixth").mouseover(function()
+    {
+      $("#hide_sixth").fadeIn("slow");
+    });
+    $(".sixth").mouseleave(function()
+    {
+      $("#hide_sixth").fadeOut("slow");
+    });
 });
